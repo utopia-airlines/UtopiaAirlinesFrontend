51c5cf43a687a93fd41f78311fe51b0d591d6a2d
Jonathan Lovelace <kingjon3377@gmail.com>
2019-06-28 16:02:23 -0400

    Replace 'bookStore.js' with stores for airline data.
    What exactly is stored, and how, may well be subject to change; the code will still not compile at
    this point.

---
+++ src/stores/flightStore.js
@@ -0,0 +1,61 @@
+import Dispatcher from '../dispatcher/appDispatcher';
+import {FLIGHT_ACTIONS} from '../actions/flightActions';
+import {EventEmitter} from 'events';
+
+const CHANGE_EVENT = 'change';
+
+let _flightStore = {
+    flights: [],
+    filteredFlights: null,
+    selected: null
+}
+
+class FlightStoreClass extends EventEmitter {
+    addChangeListener(cb) {
+        this.on(CHANGE_EVENT, cb);
+    }
+
+    removeChangeListener(cb) {
+        this.removeListener(CHANGE_EVENT, cb);
+    }
+
+    emitChange() {
+        this.emit(CHANGE_EVENT);
+    }
+
+    getAllFlights() {
+        return _flightStore.flights;
+    }
+
+    getFilteredFlights() {
+        return _flightStore.filteredFlighs;
+    }
+
+    getSelectedFlight() {
+        return _flightStore.selected;
+    }
+}
+
+const FlightStore = new FlightStoreClass();
+
+Dispatcher.register((action) => {
+    switch (action.actionType) {
+        case FLIGHT_ACTIONS.FILTER_SEARCH:
+            _flightStore.filteredFlights = action.data;
+            FlightStore.emitChange();
+            break;
+        case FLIGHT_ACTIONS.SELECT_FLIGHT:
+            _flightStore.selected = action.data;
+            FlightStore.emitChange();
+            break;
+        case FLIGHT_ACTIONS.RETURN_TO_HOMEPAGE:
+            _flightStore.filter = null;
+            _flightStore.selected = null;
+            FlightStore.emitChange();
+            break;
+        default:
+            return;
+    }
+});
+
+export default FlightStore;
---
+++ src/stores/ticketStore.js
@@ -0,0 +1,75 @@
+import Dispatcher from '../dispatcher/appDispatcher';
+import {TICKET_ACTIONS} from '../actions/ticketActions';
+import {EventEmitter} from 'events';
+
+const CHANGE_EVENT = 'change';
+
+let _ticketStore = {
+    selectedSeat: null,
+    bookedTicket: null,
+    bookingFailureReason: null,
+    paymentResult: null
+}
+
+class TicketStoreClass extends EventEmitter {
+    addChangeListener(cb) {
+        this.on(CHANGE_EVENT, cb);
+    }
+
+    removeChangeListener(cb) {
+        this.removeListener(CHANGE_EVENT, cb);
+    }
+
+    emitChange() {
+        this.emit(CHANGE_EVENT);
+    }
+
+    getSelectedSeat() {
+        return _ticketStore.selectedSeat;
+    }
+
+    getBookedTicket() {
+        return _ticketStore.bookedTicket;
+    }
+
+    getBookingFailureReason() {
+        return _ticketStore.bookingFailureReason;
+    }
+
+    getPaymentResult() {
+        return _ticketStore.paymentResult;
+    }
+}
+
+const TicketStore = new TicketStoreClass();
+
+Dispatcher.register((action) => {
+	switch (action.actionType) {
+        case TICKET_ACTIONS.SELECT_SEAT:
+            _ticketStore.selectedSeat = action.data;
+            TicketStore.emitChange();
+            break;
+        case TICKET_ACTIONS.BOOK_TICKET:
+        case TICKET_ACTIONS.SHOW_BOOKING_DETAILS:
+            _ticketStore.bookedTicket = action.data;
+            TicketStore.emitChange();
+            break;
+        case TICKET_ACTIONS.BOOKING_FAILURE:
+            _ticketStore.bookingFailureReason = action.data;
+            TicketStore.emitChange();
+            break;
+        case TICKET_ACTIONS.PAY_FOR_TICKET:
+            _ticketStore.paymentResult = action.data;
+            TicketStore.emitChange();
+            break;
+        case TICKET_ACTIONS.TIME_OUT:
+        case TICKET_ACTIONS.CANCEL:
+            _ticketStore.bookedTicket = null;
+            TicketStore.emitChange();
+            break;
+        default:
+            return;
+    }
+});
+
+export default TicketStore;
