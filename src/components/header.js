5a640fe32a8fd5004eba8fc3cf3ab4b5fa2bc8bc
Jonathan Lovelace <kingjon3377@gmail.com>
2019-07-01 15:36:31 -0400

    Convert Header component from a class to a function and replace Books links with an Enter Booking ID link.

--- src/components/header.js
+++ src/components/header.js
@@ -3,20 +3,19 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-export class Header extends React.Component{
-    render() {
-        return(
-            <nav className="navbar navbar-default">
-                <div className="container-fluid">
-                    <Link to="/" className="navbar-brand">
-                        <img width="90px" height="30px" src="images/logo.png" />
-                    </Link>
-                    <ul className="nav navbar-nav">
-                        <li><Link to="/" replace>Home</Link></li>
-                        <li><Link to="/books" replace>Books</Link></li>
-                    </ul>
-                </div>
-            </nav>
-        );
-    }
+export default function Header() {
+    return (
+        <nav className="navbar navbar-default">
+            <div className="container-fluid">
+                <Link to="/" className="navbar-brand">
+                    <img width="90px" height="30px" src="images/logo.png" />
+                </Link>
+                <ul className="nav navbar-nav">
+                    <li><Link to="/" replace>Home</Link></li>
+                    {/* TODO: make asking for booking ID a modal dialog */}
+                    <li><Link to="/enter-booking-id" replace>Enter Booking ID</Link></li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
