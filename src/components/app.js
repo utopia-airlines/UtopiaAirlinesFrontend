7fae71ff66fff8ae194537e8f4af20794ad4efda
Jonathan Lovelace <kingjon3377@gmail.com>
2019-07-03 16:07:46 -0400

    Rename Books component to Flights.

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import Header from './header';
-import {Books} from './books';
+import {Flights} from './Flights';
 import FlightStore from '../stores/flightStore';
 import {FlightList} from './FlightList';
 
@@ -13,7 +13,7 @@ export class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            bookList:[]
+            flightList:[]
         };
     }
 
@@ -23,7 +23,7 @@ export class App extends React.Component {
                 <Header />
                 <Switch>
                     <Route exact path='/' component={FlightList}/>
-                    <Route path='/books' render={(props) => (<Books {...props} bookList={this.state.bookList} />)}/>
+                    <Route path='/books' render={(props) => (<Flights {...props} flightList={this.state.flightList} />)}/>
                 </Switch>
             </div>
         );
