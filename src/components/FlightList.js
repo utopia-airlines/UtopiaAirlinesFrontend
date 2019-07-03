ac8a1d6558cb3f3b3ed4d10fb13d9085f0515166
Jonathan Lovelace <kingjon3377@gmail.com>
2019-07-03 14:25:07 -0400

    Refactor BookList component into FlightList component.

---
+++ src/components/FlightList.js
@@ -0,0 +1,52 @@
+"use strict"
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import {FlightActions} from '../actions/flightActions';
+
+
+export class FlightList extends React.Component{
+
+    createFlightRow(flight) {
+        return (
+            <tr key={flight.id}>
+                <td> {flight.id} </td>
+                <td> {flight.departure} </td>
+                <td> {flight.departureDate} </td>
+                <td> {flight.destination} </td>
+                <td> {flight.arrivalDate} </td>
+            </tr>
+        );
+    }
+
+    componentWillMount() {
+        FlightActions.filterSearch();
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Available Flights</h1>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Author</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.flightList.map(this.createFlightRow, this)}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+FlightList.propTypes = {
+    flightList: PropTypes.array.isRequired
+};
+
+
+
