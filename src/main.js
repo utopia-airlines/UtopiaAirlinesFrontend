36fe9fe9890b1d8c439e5ac90c60681aefd220c7
Jonathan Lovelace <kingjon3377@gmail.com>
2019-07-09 15:20:41 -0400

    In main.js, use BrowerRouter rather than HashRouter.

--- src/main.js
+++ src/main.js
@@ -8,12 +8,12 @@ window.$ = window.jQuery=jquery;
 
 import React from 'react';
 import ReactDom from 'react-dom';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 import {App} from './components/App.js';
 
 ReactDom.render((
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   ), document.getElementById('app'));
