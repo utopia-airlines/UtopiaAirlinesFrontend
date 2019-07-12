d93bfa63572a2a46dc8f3e3f7be6a1c6374cd08e
Jonathan Lovelace <kingjon3377@gmail.com>
2019-07-12 16:32:16 -0400

    Use HashRouter rather than BrowserRouter.
    This reverts 1bd6cf443c979701cf09d407e953d6f066f4f971.
    
    BrowserRouter allows *much* nicer URLs (and would allow indexing by search engines), but
    will not actually work when the site is served from a static file storage server like S3.

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@
 
 import React from 'react';
 import ReactDom from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 import {App} from './components/App.js';
 
 ReactDom.render((
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   ), document.getElementById('app'));
