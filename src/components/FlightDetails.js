1eeec21010c345111a4de1fedb1e113e59311d69
Jonathan Lovelace <kingjon3377@gmail.com>
2019-07-18 17:46:13 -0400

    Start integrating @rndmkid's work on flight-details component.
    This is just a start and doesn't actually work (in part, but not entirely, because
    the specific flight is never actually passed into its props), but as a start it's
    worth sharing with the rest of the team at this point.

--- src/components/FlightDetails.js
+++ src/components/FlightDetails.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table';
+import PropTypes from 'prop-types';
+import {datePart, timePart} from '../util/datetime';
 
 export class FlightDetails extends React.Component {
     render() {
-        return (<div>
+        return (
+            <div className="seat-list">
+                <div className="flight_info container">
+                    <div className="row">
+                        <div className="col-4">
+                            <div> {this.props.flight.departure} </div>
+                            <div> {datePart(this.props.flight.departure_date)} </div>
+                            <div> {timePart(this.props.flight.departure_date)} </div>
+                        </div>
+                        <div className="col-4">
+                            <img src="./resources/plane_icon_72.png" />
+                        </div>
+                        <div className="col-4">
+                            <div> {this.props.flight.destination} </div>
+                            <div> {datePart(this.props.flight.arrival_date)} </div>
+                            <div> {timePart(this.props.flight.arrival_date)} </div>
+                        </div>
+                    </div>
+                </div>
 
-        </div>);
+                <Table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Seat</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Class</th>
+                            <th scope="col" className="button-col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/*
+                        <tr>
+                            <td>1-a</td>
+                            <td>$100</td>
+                            <td>First</td>
+                            <td className="button-col">
+                                <button className="blue-btn btn btn-sm" >
+                                    Select
+                                </button>
+                            </td>
+                        </tr> */}
+                    </tbody>
+                </Table>
+            </div>);
     }
 }
+
+FlightDetails.propTypes = {
+    flight: PropTypes.object.isRequired
+};
