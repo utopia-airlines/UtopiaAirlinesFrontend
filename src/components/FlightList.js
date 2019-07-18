c1b393a5721bf16eba9361fdcf30a3bcc0214ab3
Jonathan Lovelace <kingjon3377@gmail.com>
2019-07-18 15:55:50 -0400

    Integrate much of @rndmkid's design into FlightList component.

--- src/components/FlightList.js
+++ src/components/FlightList.js
@@ -9,10 +9,12 @@ function createFlightRow(flight) {
     return (
         <tr key={flight.flight_number} onClick={() => FlightActions.selectFlight(flight)}>
             <td> {flight.flight_number} </td>
-            <td> {flight.departure} </td>
             <td> {flight.departure_date} </td>
-            <td> {flight.destination} </td>
+            <td> {flight.departure} </td>
             <td> {flight.arrival_date} </td>
+            <td> {flight.destination} </td>
+            <td> <button className="blue-btn btn button-sm">
+                Select{/* FIXME: Add handler */}</button></td>
         </tr>
     );
 }
@@ -21,14 +23,22 @@ export function FlightList(props) {
     return (
         <div>
             <h1>Available Flights</h1>
-            <Table className="table">
-                <thead>
+            <Table className="table flights-table">
+                <thead id="flight-search-bar">
                     <tr>
-                        <th>ID</th>
-                        <th>Departure</th>
-                        <th>Departure Date/Time</th>
-                        <th>Destination</th>
-                        <th>Arrival Date/Time</th>
+                        <th scope="col">{'Flight #'}</th>
+                        <th scope="colgroup" colSpan="2">Departing</th>
+                        <th scope="colgroup" colSpan="2">Arriving</th>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <th scope="col"><input type="date" id="departureDate" />&nbsp;&nbsp;from&nbsp;&nbsp;</th>
+                        <th scope="col"><input maxLength="3" id="origin" width="5" /></th>
+                        <th scope="col"><input type="date" id="arrivalDate" />&nbsp;&nbsp;to&nbsp;&nbsp;</th>
+                        <th scope="col"><input maxLength="3" id="destination" width="5" /></th>
+                        <th scope="col"><button className="orange-btn btn btn-sm" id="submitFilter">
+                            Search{/*FIXME: Add click handler*/}</button></th>
                     </tr>
                 </thead>
                 <tbody>
