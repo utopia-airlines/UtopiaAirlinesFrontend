cb6b6eb31fc295efa5879307d81691cc90550c4b
Jonathan Lovelace <kingjon3377@gmail.com>
2019-07-23 15:37:38 -0400

    Extract table header from FlightList into a separate component.

--- src/components/FlightList.js
+++ src/components/FlightList.js
@@ -23,28 +23,34 @@ function createFlightRow(flight) {
     );
 }
 
+function FlightListHeader() {
+    return (
+        <thead id="flight-search-bar">
+            <tr>
+                <th scope="col">{'Flight #'}</th>
+                <th scope="colgroup" colSpan={2}>Departing</th>
+                <th scope="colgroup" colSpan={2}>Arriving</th>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <th scope="col"><input type="date" id="departureDate" />&nbsp;&nbsp;from&nbsp;&nbsp;</th>
+                <th scope="col"><input maxLength={3} id="origin" width="5" /></th>
+                <th scope="col"><input type="date" id="arrivalDate" />&nbsp;&nbsp;to&nbsp;&nbsp;</th>
+                <th scope="col"><input maxLength={3} id="destination" width="5" /></th>
+                <th scope="col"><button className="orange-btn btn btn-sm" id="submitFilter">
+                    Search{/*FIXME: Add click handler*/}</button></th>
+            </tr>
+        </thead>
+    );
+}
+
 export function FlightList(props) {
     return (
         <div>
             <h1>Available Flights</h1>
             <Table className="table flights-table">
-                <thead id="flight-search-bar">
-                    <tr>
-                        <th scope="col">{'Flight #'}</th>
-                        <th scope="colgroup" colSpan={2}>Departing</th>
-                        <th scope="colgroup" colSpan={2}>Arriving</th>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <th scope="col"><input type="date" id="departureDate" />&nbsp;&nbsp;from&nbsp;&nbsp;</th>
-                        <th scope="col"><input maxLength={3} id="origin" width="5" /></th>
-                        <th scope="col"><input type="date" id="arrivalDate" />&nbsp;&nbsp;to&nbsp;&nbsp;</th>
-                        <th scope="col"><input maxLength={3} id="destination" width="5" /></th>
-                        <th scope="col"><button className="orange-btn btn btn-sm" id="submitFilter">
-                            Search{/*FIXME: Add click handler*/}</button></th>
-                    </tr>
-                </thead>
+                <FlightListHeader />
                 <tbody>
                     {props.flightList.map(createFlightRow)}
                 </tbody>
