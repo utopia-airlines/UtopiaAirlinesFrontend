51a2be72c9d886a9770dfc1a6199a015409bbaea
Jonathan Lovelace <kingjon3377@gmail.com>
2019-08-23 13:53:12 -0400

    FlightApi: Handle errors returned from backend API calls.

--- src/api/flightApi.js
+++ src/api/flightApi.js
@@ -2,12 +2,28 @@
 
 import axios from 'axios';
 import Config from '../config';
+import Dispatcher from '../dispatcher/appDispatcher';
+import { ERROR_ACTIONS } from '../actions/errorActions';
 
 const FlightApi = {
     getFlights: function(filter, cb) {
         // TODO: If filtering is ever exposed in REST API, use that instead of doing the filtering ourselves
-        axios.get(Config.api + '/flights').then((response) => { // TODO: What if it fails, and/or returns a non-200-series response?
-            cb(filter(response.data));
+        axios.get(Config.api + '/flights').then((response) => {
+            if (response.status && response.status >= 200 && response.status < 300) {
+                cb(filter(response.data));
+            } else {
+                Dispatcher.dispatch({
+                    type: ERROR_ACTIONS.SHOW_ERROR,
+                    value: 'Getting list of flights failed',
+                    extra: response // Not used, but useful in debugging
+                });
+            }
+        }, (err) => {
+            Dispatcher.dispatch({
+                type: ERROR_ACTIONS.SHOW_ERROR,
+                value: 'Getting list of flights failed',
+                extra: err // Not used, but useful in debugging
+            });
         });
     },
 
@@ -18,8 +34,22 @@ const FlightApi = {
         } else {
             flightNumber = flight;
         }
-        axios.get(Config.api + '/flight/' + flightNumber + '/seats').then((response) => { // TODO: What if this fails, or returns a non-200-series response?
-            cb(response.data);
+        axios.get(Config.api + '/flight/' + flightNumber + '/seats').then((response) => {
+            if (response.status && response.status >= 200 && response.status < 300) {
+                cb(response.data);
+            } else {
+                Dispatcher.dispatch({
+                    type: ERROR_ACTIONS.SHOW_ERROR,
+                    value: 'Getting list of seats failed',
+                    extra: response // Not used, but useful in debugging
+                });
+            }
+        }, (err) => {
+            Dispatcher.dispatch({
+                type: ERROR_ACTIONS.SHOW_ERROR,
+                value: 'Getting list of seats failed',
+                extra: err // Not used, but useful in debugging
+            });
         });
     }
 }
